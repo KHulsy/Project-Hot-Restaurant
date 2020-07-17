8e2f18bf694d397b80facf7a0e10953a1362d759
Sierra Chapman <siechap@gmail.com>
2020-07-17 11:12:20 -0700

    create basic server

--- Express.js
+++ Express.js
@@ -1,1 +1,12 @@
-Placeholder
+// Get dependencies
+var express = require("express");
+var path = require("path");
+
+// Set up express
+var app = express();
+var PORT = 3000;
+
+// Start the server
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
