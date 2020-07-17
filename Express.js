096bbb4b5e3c69e4aae10e0b307591b6e0111c7a
Sierra Chapman <siechap@gmail.com>
2020-07-17 11:28:04 -0700

    create arrays to store data

--- Express.js
+++ Express.js
@@ -1,10 +1,28 @@
 // Get dependencies
-var express = require("express");
-var path = require("path");
+const express = require("express");
+const path = require("path");
 
 // Set up express
-var app = express();
-var PORT = 3000;
+const app = express();
+const PORT = 3000;
+
+// Create arrays to store data
+let tables = [
+    {
+        name: "Sierra",
+        phone: "555-555-5555",
+        email: "siechap@gmail.com",
+        id: "me"
+    }
+]
+let waitlist = [
+    {
+        name: "Sample",
+        phone: "123-555-6789",
+        email: "sample@sample.com",
+        id: "sam"
+    }
+]
 
 // Start the server
 app.listen(PORT, function() {
