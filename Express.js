44bcde8eae831a305c81368a23cca5c9e97044e3
Sierra Chapman <siechap@gmail.com>
2020-07-17 12:28:24 -0700

    create API route for submitting reservation

--- Express.js
+++ Express.js
@@ -6,6 +6,10 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Handle data parsing for POST requests
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // Create arrays to store data
 let tables = [
     {
@@ -49,6 +53,22 @@ app.get("/api/tables", function(req, res) {
 // Displays the waitlist
 app.get("/api/waitlist", function(req, res) {
     return res.json(waitlist);
+});
+
+// Process submission for new reservation
+app.post("/api/tables", function(req, res) {
+    // get the info about the new reservation
+    const newReservation = req.body;
+    console.log(newReservation);
+
+    // if space in tables, add to tables; else, add to waitlist
+    if (tables.length < 5) {
+        tables.push(newReservation);
+        res.send(true);
+    } else {
+        waitlist.push(newReservation);
+        res.send(false);
+    }
 });
 
 // Start the server
